b9a4bd8620621cfe3821bb6cfbe2fae96018c2f1
catarak <ctarakajian@gmail.com>
2016-03-05 23:57:35 -0500

    add menu fade directive

---
+++ app/common-components/directives/ng-fade-menu_module.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var fadeMenuDirective = angular.module('ngFadeMenu', [ ] );
+
+fadeMenuDirective.directive('ngFadeMenu', function($timeout) 
+{
+	return {
+		restrict: 'A',
+		link: function(scope, element, attr) {
+			var timeoutPromise;
+			// resetFadeTimeout();
+			element.parent().parent().parent().parent().parent().bind('mousemove', function(e) {
+				resetFadeTimeout();
+			});
+
+			scope.$watch('stateName', function(newValue, oldValue) {
+				resetFadeTimeout();
+			});
+
+			scope.$watch('stateParams.section', function(newValue, oldValue) {
+				resetFadeTimeout();
+			});
+
+			function resetFadeTimeout() {
+				element.removeClass("hidden");
+				$timeout.cancel(timeoutPromise);
+				if (scope.stateName === "root" || (scope.stateName === "root.section-state" && scope.stateParams.section !== "news" && scope.stateParams.section !== "search")) {
+					timeoutPromise = $timeout(function(e) {
+						element.addClass("hidden");
+					}, 3000);
+				}
+			}
+
+		}
+	}
+});
