06acd825584aac33eac6ca80e684eebda2d14ca1
Cooper Mayne <coopermayne@gmail.com>
2016-03-25 17:27:09 -0400

    sort by last name

--- app/common-components/filters/sorting-type_module.js
+++ app/common-components/filters/sorting-type_module.js
@@ -22,7 +22,7 @@ sortingTypeFilter.filter( 'sortingType', function(  )
 					break;
 				case 'people':
 					items.forEach( function( item ) {
-						if( item.name.charAt( 0 ).toLowerCase(  ) === input ) {
+						if( item.last_name.charAt( 0 ).toLowerCase(  ) === input ) {
 							out.push( item );
 						}
 					} );
