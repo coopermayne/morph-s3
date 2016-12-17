12706011590b956f27dea44fa4713721f67a354b
Cooper Mayne <coopermayne@gmail.com>
2016-12-17 13:48:31 -0800

    gulp file for mobile launch

--- gulpfile.js
+++ gulpfile.js
@@ -58,3 +58,20 @@ gulp.task( 'publish', function(  )
 		'launch'
 	);
 } );
+
+gulp.task( 'publish-mobile', function(  )
+{
+	runSequence(
+		'clean',
+		[
+			'build-images',
+			'build-scripts',
+			'build-css',
+			'build-fonts'
+		],
+		'jade',
+		'build-inject',
+		'build-html',
+		'launch-mobile'
+	);
+} );
