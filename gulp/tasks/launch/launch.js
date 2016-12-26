235bb139f8b34d081ca0f8d3772fb24ae3cf3b6c
Cooper Mayne <coopermayne@gmail.com>
2016-12-25 16:59:09 -0800

    add launch and launch live tasks

--- gulp/tasks/launch/launch.js
+++ gulp/tasks/launch/launch.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var AWS = require('aws-sdk');
 var merge = require('merge-stream');
 var gulp  = require( 'gulp' );
 var path   = require( '../../paths.js' );
@@ -7,10 +8,42 @@ var awspublish = require('gulp-awspublish')
 
 gulp.task('launch', function(){
 	var publisher = awspublish.create({
-		region: 'us-east-1',
 		params: {
-			Bucket: "test.morphosis.com"
-		}
+			Bucket: "noon.morphosis.com"
+		},
+		credentials: new AWS.SharedIniFileCredentials({profile: 'morph'})
+	})
+
+	var headers = {
+		'Cache-Control': 'max-age=1, no-transform, public'
+	};
+
+	var gzip = gulp.src([
+    'build-destination/**/*.js',
+    'build-destination/**/*.html',
+    'build-destination/**/*.css'
+  ]).pipe(awspublish.gzip({ext: ''}));
+
+	var plain = gulp.src([
+    'build-destination/**/*',
+    '!build-destination/**/*.js',
+    '!build-destination/**/*.html',
+    '!build-destination/**/*.css'
+  ]);
+
+	return merge(gzip, plain)
+		.pipe(publisher.publish(headers))
+		.pipe(publisher.sync())
+		.pipe(awspublish.reporter());
+
+})
+
+gulp.task('launch-live', function(){
+	var publisher = awspublish.create({
+		params: {
+			Bucket: "www.morphosis.com"
+		},
+		credentials: new AWS.SharedIniFileCredentials({profile: 'morph'})
 	})
 
 	var headers = {
